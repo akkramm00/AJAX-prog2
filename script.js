a4d56023bb03b63647b80e05190c3183980127c5
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-05-31 12:03:51 +0200

    script adding

---
+++ script.js
@@ -0,0 +1,39 @@
+const tbody = document.getElementById('tbody');
+const students = [];
+
+const createLine = (tbody, student) => {
+  const newRow   = tbody.insertRow();
+  let counter = 0
+  for (const key in student) {
+    const newCell  = newRow.insertCell(counter);
+    const newText  = document.createTextNode(student[key]);
+    newCell.appendChild(newText);
+    counter++
+  }
+}
+
+const sendForm = () => {
+const firstName = document.querySelector('#firstName').value
+const lastName = document.querySelector('#lastName').value
+const grade = document.querySelector('#grade').value
+const numberClass = document.querySelector('#numberClass').value
+
+const student = {id: '', firstName, lastName, grade, numberClass};
+const xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://reqres.in/api/users');
+
+xhr.addEventListener('readystatechange', () => {
+if(xhr.readyState === 4) {
+if (xhr.status === 201) {
+const data = JSON.parse(xhr.response);
+student.id = data.id;
+students.push(student);
+createLine(tbody, student);
+} else {
+alert('Erreur')
+}
+}
+});
+  
+  xhr.send(student);
+}
